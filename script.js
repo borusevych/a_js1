6f94bd45ac9346e90b63b98bddb0ed24f56e8bc5
Vanda Borusevych <borusevvych.v@gmail.com>
2023-12-22 21:32:15 +0200

    add: some changes

--- script.js
+++ script.js
@@ -1,28 +1,40 @@
 class Employee {
     constructor(name, age, salary){
-        this.name = name;
-        this.age = age;
-        this.salary = salary;
+        this._name = name;
+        this._age = age;
+        this._salary = salary;
     }
-    get employee(){
-        return `name: ${this.name}, age: ${this.age}, salary: ${this.salary}`
+    get name(){
+        return this._name
     }
-    set employee (value){
-        this.salary = value;
+    get age(){
+        return this._age
+    }
+    get salary(){
+        return this._salary
+    }
+    set name (value){
+        this._name = value;
+    }
+    set age (value){
+        this._age = value;
+    }
+    set salary (value){
+        this._salary = value;
     }
 }
 
 class Programmer extends Employee{
     constructor(name, age, salary, lang){
         super(name, age, salary);
-        this.lang = lang;
+        this._lang = lang;
     }
-    get employee(){
-        return `name: ${this.name}, age: ${this.age}, salary: ${this.salary * 3}, lang: ${this.lang}`
+    get salary(){
+        return this._salary * 3
     }
 }
 
 let employee1 = new Programmer('Doanld', 33, 1000, 'js');
 let employee2 = new Programmer('Trump', 77, 35000, 'js');
-console.log(employee1.employee);
-console.log(employee2.employee);
+console.log(employee1.name);
+console.log(employee2.salary);
